ea3990bac33a44d5b80843d158d3752390ae929c
BluDay <19403893+bluday@users.noreply.github.com>
2025-05-13 19:03:33 +0200

    chore: update global using statements

--- src/BluDay.FluentNoiseRemover/Usings.cs
+++ src/BluDay.FluentNoiseRemover/Usings.cs
@@ -1,14 +1,11 @@
-﻿global using BluDay.FluentNoiseRemover;
-
-global using Microsoft.UI.Input;
-global using Microsoft.UI.Windowing;
+﻿global using Microsoft.UI.Windowing;
 global using Microsoft.UI.Xaml;
 global using Microsoft.UI.Xaml.Controls;
-global using Microsoft.UI.Xaml.Controls.Primitives;
 
 global using System;
-global using System.Collections.ObjectModel;
-global using System.Linq;
+global using System.Diagnostics;
+global using System.Runtime.InteropServices;
 
-global using Windows.Foundation;
 global using Windows.Graphics;
+
+global using WinRT.Interop;
