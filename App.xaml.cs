85085c1b9af395bc859ac57fb6185edc04263187
BluDay <19403893+bluday@users.noreply.github.com>
2025-05-14 11:24:00 +0200

    feat: add `App.UnhandledException` event handler for debug mode

--- App.xaml.cs
+++ App.xaml.cs
@@ -6,15 +6,31 @@
 public partial class App : Application
 {
     /// <summary>
-    /// Initializes the singleton application object. This is the first line of authored code
-    /// executed, and as such is the logical equivalent of main() or WinMain().
+    /// Initializes the singleton application object. This is the first line of authored
+    /// code executed, and as such is the logical equivalent of main() or WinMain().
     /// </summary>
-    public App() => InitializeComponent();
+    public App()
+    {
+        InitializeComponent();
+
+#if DEBUG
+        UnhandledException += App_UnhandledException;
+#endif
+    }
+
+#if DEBUG
+    private void App_UnhandledException(object sender, Microsoft.UI.Xaml.UnhandledExceptionEventArgs e)
+    {
+        Debug.WriteLine($"{e.Exception}: {e.Message}");
+    }
+#endif
 
     /// <summary>
     /// Invoked when the application is launched.
     /// </summary>
-    /// <param name="args">Details about the launch request and process.</param>
+    /// <param name="args">
+    /// Details about the launch request and process.
+    /// </param>
     protected override void OnLaunched(LaunchActivatedEventArgs args)
     {
         new MainWindow().Activate();
