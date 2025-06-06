b7cea8fe793c1a35b02382b3db72f9edc8b5033c
BluDay <19403893+bluday@users.noreply.github.com>
2025-06-06 18:27:11 +0200

    feat: expose new bounding box methods in `TitleBar`

--- Controls/TitleBar.xaml.cs
+++ Controls/TitleBar.xaml.cs
@@ -5,13 +5,10 @@ namespace BluDay.FluentNoiseRemover.Controls;
 /// </summary>
 public sealed partial class TitleBar : UserControl
 {
-    #region Events
     public event EventHandler? CloseButtonClick;
 
     public event EventHandler? SettingsButtonClick;
-    #endregion
 
-    #region Constructor
     /// <summary>
     /// Initializes a new instance of the <see cref="TitleBar"/> class.
     /// </summary>
@@ -19,17 +16,24 @@ public sealed partial class TitleBar : UserControl
     {
         InitializeComponent();
     }
-    #endregion
 
-    #region Event handlers
     private void CloseButton_Click(object sender, RoutedEventArgs e)
     {
         CloseButtonClick?.Invoke(this, EventArgs.Empty);
     }
 
-    private void SettigsButton_Click(object sender, RoutedEventArgs e)
+    private void SettingsButton_Click(object sender, RoutedEventArgs e)
     {
         SettingsButtonClick?.Invoke(this, EventArgs.Empty);
     }
-    #endregion
+
+    public RectInt32 GetBoundingRectForCloseButton(double scaleFactor)
+    {
+        return CloseButton.GetBoundingBox(scaleFactor);
+    }
+
+    public RectInt32 GetBoundingRectForSettingsButton(double scaleFactor)
+    {
+        return SettingsButton.GetBoundingBox(scaleFactor);
+    }
 }
