8ce234dfcd7a39642c20c2e4dc8cb69ada909136
BluDay <19403893+bluday@users.noreply.github.com>
2025-06-11 21:28:06 +0200

    feat: add visual states to `MainControlPanel`

--- Controls/MainControlPanel.xaml.cs
+++ Controls/MainControlPanel.xaml.cs
@@ -6,7 +6,54 @@ namespace BluDay.FluentNoiseRemover.Controls;
 public sealed partial class MainControlPanel : UserControl
 {
     /// <summary>
+    /// Identifies the <see cref="IsPlaying"/> dependency property.
+    /// </summary>
+    public static readonly DependencyProperty IsPlayingProperty = DependencyProperty.Register(
+        nameof(IsPlaying),
+        typeof(bool),
+        typeof(MainControlPanel),
+        new PropertyMetadata(
+            defaultValue:            false,
+            propertyChangedCallback: OnIsPlayingChanged
+        )
+    );
+
+    /// <summary>
+    /// Gets or sets a value indicating whether the playback is currently active.
+    /// </summary>
+    public bool IsPlaying
+    {
+        get => (bool)GetValue(IsPlayingProperty);
+        set => SetValue(IsPlayingProperty, value);
+    }
+
+    /// <summary>
+    /// Invokes when the playback button has been clicked.
+    /// </summary>
+    public event EventHandler PlaybackButtonClicked;
+
+    /// <summary>
     /// Initializes a new instance of the <see cref="MainControlPanel"/> class.
     /// </summary>
-    public MainControlPanel() => InitializeComponent();
+    public MainControlPanel()
+    {
+        PlaybackButtonClicked = (sender, e) => { };
+
+        InitializeComponent();
+    }
+
+    private void UpdatePlaybackVisualState()
+    {
+        VisualStateManager.GoToState(this, IsPlaying ? "Playing" : "Normal", true);
+    }
+
+    private void PlaybackButton_Click(object sender, RoutedEventArgs e)
+    {
+        PlaybackButtonClicked?.Invoke(this, EventArgs.Empty);
+    }
+
+    private static void OnIsPlayingChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        ((MainControlPanel)d).UpdatePlaybackVisualState();
+    }
 }
