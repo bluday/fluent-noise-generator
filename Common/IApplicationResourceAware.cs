e8048e15afea24a64b68a0016247692f7e9bb8f2
BluDay <19403893+bluday@users.noreply.github.com>
2025-06-16 01:06:50 +0200

    feat: add resource loader interface to improve loose coupling

---
+++ Common/IApplicationResourceAware.cs
@@ -0,0 +1,13 @@
+﻿namespace BluDay.FluentNoiseRemover.Common;
+
+/// <summary>
+/// Defines an interface for objects that provide access to application resources.
+/// </summary>
+public interface IApplicationResourceAware
+{
+    /// <summary>
+    /// Gets the resource loader instance, which provides localized resources for
+    /// the application.
+    /// </summary>
+    ResourceLoader ResourceLoader { get; }
+}
