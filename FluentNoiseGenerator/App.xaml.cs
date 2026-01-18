c3c7edc1934a5edd0b9cfeb00f3304460954c99e
BluDay <19403893+bluday@users.noreply.github.com>
2026-01-18 14:24:31 +0100

    refactor: move container setup to a separate class

--- FluentNoiseGenerator/App.xaml.cs
+++ FluentNoiseGenerator/App.xaml.cs
@@ -1,11 +1,5 @@
 using CommunityToolkit.Mvvm.DependencyInjection;
-using CommunityToolkit.Mvvm.Messaging;
-using FluentNoiseGenerator.Common;
-using FluentNoiseGenerator.Common.Resources;
 using FluentNoiseGenerator.Common.Services;
-using FluentNoiseGenerator.Core.Services;
-using FluentNoiseGenerator.UI.Playback.Windows;
-using FluentNoiseGenerator.UI.Settings.Windows;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.UI.Xaml;
 using System;
@@ -18,7 +12,7 @@ namespace FluentNoiseGenerator;
 public partial class App : Application
 {
     #region Fields
-    private readonly ServiceProvider _rootServiceProvider;
+    private readonly Container _container;
 
     private readonly WindowService _windowService;
     #endregion
@@ -29,11 +23,13 @@ public partial class App : Application
     /// </summary>
     public App()
     {
-        _rootServiceProvider = CreateContainer();
+        _container = new Container();
 
-        Ioc.Default.ConfigureServices(_rootServiceProvider);
+        IServiceProvider rootServiceProvider = _container.RootServiceProvider;
 
-        _windowService = _rootServiceProvider.GetRequiredService<WindowService>();
+        _windowService = rootServiceProvider.GetRequiredService<WindowService>();
+
+        Ioc.Default.ConfigureServices(rootServiceProvider);
 
         InitializeComponent();
     }
@@ -49,35 +45,6 @@ public partial class App : Application
     protected override void OnLaunched(LaunchActivatedEventArgs args)
     {
         _windowService.ShowPlaybackWindow();
-    }
-    #endregion
-
-    #region Static methods
-    private static ServiceProvider CreateContainer()
-    {
-        ServiceCollection services = new();
-
-        services.AddSingleton<IMessenger>(WeakReferenceMessenger.Default);
-
-        services.AddSingleton<AppResources>();
-
-        services.AddSingleton<IAppSettings>(
-            serviceProvider => serviceProvider
-                .GetRequiredService<SettingsService>()
-                .CurrentSettings
-        );
-
-        services.AddSingleton<PlaybackWindowFactory>();
-        services.AddSingleton<SettingsWindowFactory>();
-
-        services.AddSingleton<LanguageService>();
-        services.AddSingleton<LocalizationService>();
-        services.AddSingleton<NoisePlaybackService>();
-        services.AddSingleton<SettingsService>();
-        services.AddSingleton<ThemeService>();
-        services.AddSingleton<WindowService>();
-
-        return services.BuildServiceProvider();
     }
     #endregion
 }
---
+++ FluentNoiseGenerator/Container.cs
@@ -0,0 +1,84 @@
+﻿using CommunityToolkit.Mvvm.DependencyInjection;
+using CommunityToolkit.Mvvm.Messaging;
+using FluentNoiseGenerator.Common;
+using FluentNoiseGenerator.Common.Resources;
+using FluentNoiseGenerator.Common.Services;
+using FluentNoiseGenerator.Core.Services;
+using FluentNoiseGenerator.UI.Playback.Windows;
+using FluentNoiseGenerator.UI.Settings.Windows;
+using Microsoft.Extensions.DependencyInjection;
+using System.Collections.Generic;
+
+namespace FluentNoiseGenerator;
+
+/// <summary>
+/// Represents the DI (Dependency Injection) container for the client, providing access
+/// to the root service provider, a collection of all registered services, and a method
+/// to create new service scopes for service resolution.
+/// </summary>
+internal sealed class Container
+{
+    private readonly ServiceProvider _rootServiceProvider;
+
+    /// <summary>
+    /// Gets the <see cref="ServiceProvider"/> instance for the root scope of the container.
+    /// </summary>
+    internal IKeyedServiceProvider RootServiceProvider => _rootServiceProvider;
+
+    /// <summary>
+    /// Gets a read-only collection of all service descriptors that have been registered
+    /// witin the container, providing information about the available services.
+    /// </summary>
+    internal IReadOnlyCollection<ServiceDescriptor> RegisteredServices { get; }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Container"/> class.
+    /// </summary>
+    internal Container()
+    {
+        ServiceCollection services = new();
+
+        ConfigureServices(services);
+
+        _rootServiceProvider = services.BuildServiceProvider();
+
+        RegisteredServices = services.AsReadOnly();
+
+        Ioc.Default.ConfigureServices(_rootServiceProvider);
+    }
+
+    private static void ConfigureServices(IServiceCollection services)
+    {
+        services.AddSingleton<IMessenger>(WeakReferenceMessenger.Default);
+
+        services.AddSingleton<AppResources>();
+
+        services.AddSingleton<IAppSettings>(
+            serviceProvider => serviceProvider
+                .GetRequiredService<SettingsService>()
+                .CurrentSettings
+        );
+
+        services.AddSingleton<PlaybackWindowFactory>();
+        services.AddSingleton<SettingsWindowFactory>();
+
+        services.AddSingleton<LanguageService>();
+        services.AddSingleton<LocalizationService>();
+        services.AddSingleton<NoisePlaybackService>();
+        services.AddSingleton<SettingsService>();
+        services.AddSingleton<ThemeService>();
+        services.AddSingleton<WindowService>();
+    }
+
+    /// <summary>
+    /// Creates a new limited scope for resolving services, allowing for isolated
+    /// dependencies within a specific context or operation.
+    /// </summary>
+    /// <returns>
+    /// An instance of <see cref="IServiceScope"/> representing the new scope.
+    /// </returns>
+    internal IServiceScope CreateScope()
+    {
+        return _rootServiceProvider.CreateScope();
+    }
+}
