08eed78756c55cc7fd12280f74da258586b6028d
BluDay <19403893+bluday@users.noreply.github.com>
2026-02-14 13:53:38 +0100

    feat(common): add new message for issuing notifications

---
+++ FluentNoiseGenerator.Common/Messages/IssueToastNotificationMessage.cs
@@ -0,0 +1,15 @@
+﻿namespace FluentNoiseGenerator.Common.Messages;
+
+/// <summary>
+/// Represents a message for issuing a toast notification to the operating system.
+/// </summary>
+/// <param name="Title">
+/// The title or first text element of the notification.
+/// </param>
+/// <param name="Content">
+/// The content or second text element of the notification. Is nullable.
+/// </param>
+public sealed record IssueToastNotificationMessage(
+    string  Title,
+    string? Content = null
+);
