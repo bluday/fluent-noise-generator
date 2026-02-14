dd076683d82b9b5f9bcae8b9811916d19ce1d8c2
BluDay <19403893+bluday@users.noreply.github.com>
2026-02-14 12:22:34 +0100

    feat(common): add incomplete toast notification service

---
+++ FluentNoiseGenerator.Common/Services/IToastNotificationService.cs
@@ -0,0 +1,15 @@
+﻿namespace FluentNoiseGenerator.Common.Services;
+
+/// <summary>
+/// Service for sending toast notifications to the operating system.
+/// </summary>
+public interface IToastNotificationService
+{
+    /// <summary>
+    /// Sends the specified toast notification.
+    /// </summary>
+    /// <returns>
+    /// A task representing the asynchronous operation.
+    /// </returns>
+    Task SendAsync();
+}
---
+++ FluentNoiseGenerator.Common/Services/ToastNotificationService.cs
@@ -0,0 +1,49 @@
+﻿using CommunityToolkit.Mvvm.Messaging;
+
+namespace FluentNoiseGenerator.Common.Services;
+
+/// <summary>
+/// Default implementation for the <see cref="IToastNotificationService"/> service.
+/// </summary>
+public sealed class ToastNotificationService : IToastNotificationService, IDisposable
+{
+    #region Fields
+    private readonly IMessenger _messenger;
+    #endregion
+
+    #region Constructor
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ToastNotificationService"/> class.
+    /// </summary>
+    /// <param name="messenger">
+    /// The messenger instance used for sending messages within the application.
+    /// </param>
+    /// <exception cref="ArgumentNullException">
+    /// Throws when any of the parameters is <c>null</c>.
+    /// </exception>
+    public ToastNotificationService(IMessenger messenger)
+    {
+        ArgumentNullException.ThrowIfNull(messenger);
+
+        _messenger = messenger;
+
+        RegisterMessageHandlers();
+    }
+    #endregion
+
+    #region Methods
+    private void RegisterMessageHandlers() { }
+
+    /// <inheritdoc cref="IDisposable.Dispose()"/>
+    public void Dispose()
+    {
+        _messenger.UnregisterAll(this);
+    }
+
+    /// <inheritdoc cref="IToastNotificationService.SendAsync()"/>
+    public Task SendAsync()
+    {
+        return Task.CompletedTask;
+    }
+    #endregion
+}
