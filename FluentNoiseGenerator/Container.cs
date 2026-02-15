911e97734de5f56a838ed87e34557156622f1a2d
BluDay <19403893+bluday@users.noreply.github.com>
2026-02-15 21:42:32 +0100

    feat: add new properties to `Container`

--- FluentNoiseGenerator/Container.cs
+++ FluentNoiseGenerator/Container.cs
@@ -8,7 +8,9 @@ using FluentNoiseGenerator.UI.Playback.Windows;
 using FluentNoiseGenerator.UI.Settings.ViewModels;
 using FluentNoiseGenerator.UI.Settings.Windows;
 using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace FluentNoiseGenerator;
 
@@ -17,9 +19,24 @@ namespace FluentNoiseGenerator;
 /// to the root service provider, a collection of all registered services, and a method
 /// to create new service scopes for service resolution.
 /// </summary>
-internal sealed class Container
+internal sealed partial class Container : IDisposable
 {
+    private bool _isDisposed;
+
     private readonly ServiceProvider _rootServiceProvider;
+
+    private readonly Collection<IServiceScope> _scopes;
+
+    /// <summary>
+    /// Gets a value indicating whether the container has been disposed.
+    /// </summary>
+    public bool IsDisposed => _isDisposed;
+
+    /// <summary>
+    /// Gets a read-only collection of all service descriptors that have been registered
+    /// witin the container, providing information about the available services.
+    /// </summary>
+    internal IReadOnlyCollection<ServiceDescriptor> RegisteredServices { get; }
 
     /// <summary>
     /// Gets the <see cref="ServiceProvider"/> instance for the root scope of the container.
@@ -27,10 +44,9 @@ internal sealed class Container
     internal IKeyedServiceProvider RootServiceProvider => _rootServiceProvider;
 
     /// <summary>
-    /// Gets a read-only collection of all service descriptors that have been registered
-    /// witin the container, providing information about the available services.
+    /// Gets a read-only collection of all scopes.
     /// </summary>
-    internal IReadOnlyCollection<ServiceDescriptor> RegisteredServices { get; }
+    internal IReadOnlyCollection<IServiceScope> Scopes => _scopes.AsReadOnly();
 
     /// <summary>
     /// Initializes a new instance of the <see cref="Container"/> class.
@@ -42,6 +58,8 @@ internal sealed class Container
         ConfigureServices(services);
 
         _rootServiceProvider = services.BuildServiceProvider();
+
+        _scopes = [];
 
         RegisteredServices = services.AsReadOnly();
 
@@ -82,8 +100,37 @@ internal sealed class Container
     /// <returns>
     /// An instance of <see cref="IServiceScope"/> representing the new scope.
     /// </returns>
+    /// <exception cref="InvalidOperationException">
+    /// Throws if the current instance has been disposed of.
+    /// </exception>
     internal IServiceScope CreateScope()
     {
-        return _rootServiceProvider.CreateScope();
+        if (_isDisposed)
+        {
+            throw new InvalidOperationException();
+        }
+
+        IServiceScope scope = _rootServiceProvider.CreateScope();
+
+        _scopes.Add(scope);
+
+        return scope;
+    }
+
+    /// <inheritdoc cref="IDisposable.Dispose()"/>
+    /// <remarks>
+    /// Automatically disposes of all services within all active scopes before
+    /// disposing of each scope.
+    /// </remarks>
+    public void Dispose()
+    {
+        if (_isDisposed) return;
+
+        foreach (IServiceScope scope in _scopes)
+        {
+            scope?.Dispose();
+        }
+
+        _isDisposed = true;
     }
 }
