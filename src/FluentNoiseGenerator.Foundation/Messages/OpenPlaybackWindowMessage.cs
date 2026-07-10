fe3aacff33197e086fbb43028fbb42cd43e689b7
BluDay <19403893+bluday@users.noreply.github.com>
2026-07-10 07:19:51 +0200

    feat(foundation): add new `OpenPlaybackWindowMessage` message

---
+++ src/FluentNoiseGenerator.Foundation/Messages/OpenPlaybackWindowMessage.cs
@@ -0,0 +1,6 @@
+﻿namespace FluentNoiseGenerator.Foundation.Messages;
+
+/// <summary>
+/// Represents a message for opening the playback window.
+/// </summary>
+public sealed record OpenPlaybackWindowMessage();
