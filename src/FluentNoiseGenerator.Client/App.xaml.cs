1f69e75941e00a6db4a93fceac18468edbea87f8
BluDay <19403893+bluday@users.noreply.github.com>
2026-09-02 22:48:41 +0200

    refactor(client): simplify window management

--- src/FluentNoiseGenerator.Client/App.xaml.cs
+++ src/FluentNoiseGenerator.Client/App.xaml.cs
@@ -1,8 +1,5 @@
 using CommunityToolkit.Mvvm.Messaging;
-using FluentNoiseGenerator.Features.Playback.UI.Windows;
-using FluentNoiseGenerator.Features.Settings.UI.Windows;
 using FluentNoiseGenerator.Foundation.Messages;
-using FluentNoiseGenerator.UI.Windowing;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.UI.Xaml;
 
@@ -14,11 +11,9 @@ namespace FluentNoiseGenerator.Client;
 public sealed partial class App : Application
 {
     #region Instance fields
-    private PlaybackWindow? _playbackWindow;
-
-    private SettingsWindow? _settingsWindow;
-
     private readonly ServiceProvider _rootServiceProvider;
+
+    private readonly WindowService _windowService;
     #endregion
 
     #region Constructor
@@ -29,49 +24,13 @@ public sealed partial class App : Application
     {
         _rootServiceProvider = ServiceProviderFactory.Create();
 
-        RegisterMessageHandlers();
+        _windowService = new WindowService();
 
         InitializeComponent();
     }
     #endregion
 
-    #region Message handlers
-    private void HandleClosePlaybackWindowMessage(object sender, ClosePlaybackWindowMessage message)
-    {
-        CloseWindow(ref _playbackWindow);
-    }
-
-    private void HandleCloseSettingsWindowMessage(object sender, CloseSettingsWindowMessage message)
-    {
-        CloseWindow(ref _settingsWindow);
-    }
-
-    private void HandleOpenPlaybackWindowMessage(object sender, OpenPlaybackWindowMessage message)
-    {
-        CreateAndActivateWindow(ref _playbackWindow);
-    }
-
-    private void HandleOpenSettingsWindowMessage(object sender, OpenSettingsWindowMessage message)
-    {
-        CreateAndActivateWindow(ref _settingsWindow);
-    }
-    #endregion
-
     #region Instance methods
-    private void RegisterMessageHandlers()
-    {
-        Subscribe<ClosePlaybackWindowMessage>(HandleClosePlaybackWindowMessage);
-        Subscribe<CloseSettingsWindowMessage>(HandleCloseSettingsWindowMessage);
-        Subscribe<OpenPlaybackWindowMessage>(HandleOpenPlaybackWindowMessage);
-        Subscribe<OpenSettingsWindowMessage>(HandleOpenSettingsWindowMessage);
-    }
-
-    private void Subscribe<TMessage>(MessageHandler<object, TMessage> handler)
-        where TMessage : class
-    {
-        WeakReferenceMessenger.Default.Register(this, handler);
-    }
-
     /// <summary>
     /// Invoked when the application is launched.
     /// </summary>
@@ -80,48 +39,7 @@ public sealed partial class App : Application
     /// </param>
     protected override void OnLaunched(LaunchActivatedEventArgs e)
     {
-        CreateAndActivateWindow(ref _playbackWindow);
-    }
-    #endregion
-
-    #region Static methods
-    private static bool CloseWindow<TWindow>(ref TWindow? window)
-        where TWindow : Window
-    {
-        if (window is null) return false;
-
-        window.Close();
-
-        window = null;
-
-        return true;
-    }
-
-    private static void CreateAndActivateWindow<TWindow>(ref TWindow? window)
-        where TWindow : Window, new()
-    {
-        if (CreateWindow(ref window))
-        {
-            window!.Activate();
-        }
-    }
-
-    private static bool CreateWindow<TWindow>(ref TWindow? window)
-        where TWindow : Window, new()
-    {
-        if (window is not null)
-        {
-            return false;
-        }
-
-        window = new();
-
-        if (window is IConfigurableWindow configurableWindow)
-        {
-            configurableWindow.ApplyConfiguration();
-        }
-
-        return true;
+        _windowService.OpenPlaybackWindow();
     }
     #endregion
 }
---
+++ src/FluentNoiseGenerator.Client/WindowService.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using CommunityToolkit.Mvvm.Messaging;
+using FluentNoiseGenerator.Features.Playback.UI.Windows;
+using FluentNoiseGenerator.Features.Settings.UI.Windows;
+using FluentNoiseGenerator.Foundation.Messages;
+using FluentNoiseGenerator.UI.Windowing;
+using Microsoft.UI.Xaml;
+
+namespace FluentNoiseGenerator.Client;
+
+/// <summary>
+/// Represents a service for managing windows within the application.
+/// </summary>
+public sealed partial class WindowService : IDisposable
+{
+    #region Instance fields
+    private bool _isDisposed;
+
+    private PlaybackWindow? _playbackWindow;
+
+    private SettingsWindow? _settingsWindow;
+    #endregion
+
+    #region Instance constructor
+    /// <summary>
+    /// Initializes a new instance of the <see cref="WindowService"/>
+    /// class.
+    /// </summary>
+    public WindowService()
+    {
+        RegisterMessageHandlers();
+    }
+    #endregion
+
+    #region Instance methods
+    public void ClosePlaybackWindow()
+    {
+        CloseWindow(ref _playbackWindow);
+    }
+
+    public void CloseSettingsWindow()
+    {
+        CloseWindow(ref _settingsWindow);
+    }
+
+    /// <inheritdoc/>
+    public void Dispose()
+    {
+        if (_isDisposed) return;
+
+        WeakReferenceMessenger.Default.UnregisterAll(this);
+
+        _isDisposed = true;
+    }
+
+    public void OpenPlaybackWindow()
+    {
+        CreateAndActivateWindow(ref _playbackWindow);
+    }
+
+    public void OpenSettingsWindow()
+    {
+        CreateAndActivateWindow(ref _settingsWindow);
+    }
+
+    private void RegisterMessageHandlers()
+    {
+        Subscribe<ClosePlaybackWindowMessage>(
+            (sender, message) => ClosePlaybackWindow()
+        );
+
+        Subscribe<CloseSettingsWindowMessage>(
+            (sender, message) => CloseSettingsWindow()
+        );
+
+        Subscribe<OpenPlaybackWindowMessage>(
+            (sender, message) => OpenPlaybackWindow()
+        );
+
+        Subscribe<OpenSettingsWindowMessage>(
+            (sender, message) => OpenSettingsWindow()
+        );
+    }
+
+    private void Subscribe<TMessage>(MessageHandler<object, TMessage> handler)
+        where TMessage : class
+    {
+        WeakReferenceMessenger.Default.Register(this, handler);
+    }
+    #endregion
+
+    #region Static methods
+    private static bool CloseWindow<TWindow>(ref TWindow? window)
+        where TWindow : Window
+    {
+        if (window is null) return false;
+
+        window.Close();
+
+        window = null;
+
+        return true;
+    }
+
+    private static void CreateAndActivateWindow<TWindow>(ref TWindow? window)
+        where TWindow : Window, new()
+    {
+        if (CreateWindow(ref window))
+        {
+            window!.Activate();
+        }
+    }
+
+    private static bool CreateWindow<TWindow>(ref TWindow? window)
+        where TWindow : Window, new()
+    {
+        if (window is not null)
+        {
+            return false;
+        }
+
+        window = new();
+
+        if (window is IConfigurableWindow configurableWindow)
+        {
+            configurableWindow.ApplyConfiguration();
+        }
+
+        return true;
+    }
+    #endregion
+}
